3bb3bbcce1a0989aa00d4ab2f8aeaf27bcc78ac9
dducktai <dangductai6410@gmail.com>
2024-05-26 17:56:18 +0700

    ABAC

---
+++ ABAC/src/abac/resource.py
@@ -0,0 +1,5 @@
+class Resource:
+    def __init__(self, resource_name, sensitivity, resource_type):
+        self.resource_name = resource_name
+        self.sensitivity = sensitivity
+        self.resource_type = resource_type
---
+++ ABAC/src/abac/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username, role, department):
+        self.username = username
+        self.role = role
+        self.department = department
