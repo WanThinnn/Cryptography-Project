36918f346557d011815d94fe939831dd6132eeb7
dducktai <dangductai6410@gmail.com>
2024-06-09 12:41:03 +0700

    ABAC gui final

---
+++ main/ABAC/abac.py
@@ -0,0 +1,49 @@
+#abac.py
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(617, 401)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        
+        self.lbUsername = QtWidgets.QLabel(parent=self.centralwidget)
+        self.lbUsername.setGeometry(QtCore.QRect(30, 30, 200, 17))
+        self.lbUsername.setObjectName("lbUsername")
+        
+        self.cbResource = QtWidgets.QComboBox(parent=self.centralwidget)
+        self.cbResource.setGeometry(QtCore.QRect(150, 70, 241, 25))
+        self.cbResource.setObjectName("cbResource")
+        # self.cbResource.addItem("")
+        # self.cbResource.addItem("")
+        # self.cbResource.addItem("")
+        # self.cbResource.addItem("")
+        # self.cbResource.addItem("")
+        
+        self.btnOK = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.btnOK.setGeometry(QtCore.QRect(390, 70, 51, 25))
+        self.btnOK.setObjectName("btnOK")
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 617, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.lbUsername.setText(_translate("MainWindow", "Welcome"))
+        # self.cbResource.setItemText(0, _translate("MainWindow", "students"))
+        # self.cbResource.setItemText(1, _translate("MainWindow", "customers"))
+        # self.cbResource.setItemText(2, _translate("MainWindow", "staffs"))
+        # self.cbResource.setItemText(3, _translate("MainWindow", "customers"))
+        # self.cbResource.setItemText(4, _translate("MainWindow", "partners"))
+        self.btnOK.setText(_translate("MainWindow", "OK"))
---
+++ main/ABAC/fabac.py
@@ -0,0 +1,51 @@
+#fabac.py
+from classABAC import AttributeBasedAccessControl
+import mysql.connector
+
+
+
+class ProcessMongoDBCloud:
+    def __init__(self):
+        self.host = "company-database.clce6ae44hhz.ap-southeast-2.rds.amazonaws.com"
+        self.user = "admin"
+        self.password = "24122003"
+        self.database = "company_db"
+        self.connection = self.connect_to_db()
+        self.plaintext_file = None
+        self.encrypted_file = None
+        self.decrypted_file = None
+        self.keyfile = None
+
+    def connect_to_db(self):
+        return mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+    def handle_abac(username, selected_resource):
+        # Giả sử bạn có một instance của AttributeBasedAccessControl
+        abac_instance = AttributeBasedAccessControl()
+        employee_attributes = abac_instance.get_employee_attributes(username)
+        print(f"Employee attributes: {employee_attributes}")
+        # Logic xử lý ABAC tiếp theo
+        return employee_attributes
+    
+    def get_columns(self, table_name):
+        cursor = self.connection.cursor()
+        cursor.execute(f"SHOW COLUMNS FROM `{table_name}`")
+        columns = [column[0] for column in cursor.fetchall()]
+        cursor.close()
+        return columns
+    
+    def get_tables(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SHOW TABLES")
+        tables = [table[0] for table in cursor.fetchall()]
+        cursor.close()
+        
+        # Lọc ra các bảng có tên "login_table" và các bảng có tên bắt đầu bằng "key_"
+        filtered_tables = [table for table in tables if "key" not in table.lower() and table != "login_table"]
+            
+        return filtered_tables
