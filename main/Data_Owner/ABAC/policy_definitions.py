b3978dbea55218b97b06f3d4af67942b03a62779
WanThinnn <thienlai159@gmail.com>
2024-06-19 18:30:35 +0700

    test

--- main/Data_Owner/ABAC/policy_definitions.py
+++ main/Data_Owner/ABAC/policy_definitions.py
@@ -46,6 +46,54 @@ doctor_policy_json = {
     "priority": 1
 }
 
+master_doctor_policy_json = {
+    "uid": "3",
+    "description": "Master Doctor can view and upload all documents of their hospital.",
+    "effect": "allow",
+    "rules": {
+        "subject": {
+            "$.role": {
+                "condition": "Equals",
+                "value": "Head of Cardiology"
+            },
+            "$.department": {
+                "condition": "Equals",
+                "value": "cardiology"
+            },
+            "$.position": {
+                "condition": "Equals",
+                "value": "doctor"
+            },
+        },
+            
+        "resource": {
+            "$.type": {
+                "condition": "Exists"
+                
+            },
+            "$.department": {
+                "condition": "Exists"
+               
+            },
+        },
+        "action": {
+            "$.method": [
+            {
+                "condition": "Equals",
+                "value": "view"
+            },
+            {
+                "condition": "Equals",
+                "value": "upload"
+            }
+            ]
+        },
+        "context": {}
+    },
+    "targets": {},
+    "priority": 3
+}
+
 nurse_policy_json = {
     "uid": "2",
     "description": "Nurse can view all documents of their hospital.",
